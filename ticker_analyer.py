8cab06f7e373e1f46dd523fd4a31ad7cdcf4de59
mrsamsonn <andrewsj.ahs@gmail.com>
2023-09-17 17:30:12 -0700

    plot static adj price

--- ticker_analyer.py
+++ ticker_analyer.py
@@ -1,6 +1,7 @@
 # from yahoo_fin.stock_info import *
 import pandas as pd
 import matplotlib.pyplot as plt
+import yfinance as yf
 
 # while get_live_price('SPY') != '':
 #     print(get_live_price('SPY'))
@@ -8,4 +9,10 @@ import matplotlib.pyplot as plt
 #################
 #test static analysis first
 
+data = yf.download('SPY')
+print(data)
+
+
+plt.plot(data.index, data['Adj Close'])
+plt.show()
 
