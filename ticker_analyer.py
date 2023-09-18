cbb0e14398e5d370ad6c33b21eb2c4e0d2f873bf
mrsamsonn <andrewsj.ahs@gmail.com>
2023-09-18 15:47:57 -0700

    function cleanup

--- ticker_analyer.py
+++ ticker_analyer.py
@@ -18,21 +18,25 @@ data['ema50'] = ta.EMA(data["Adj Close"], 50)
 data['ema200'] = ta.EMA(data["Adj Close"], 200)
 data['crossing'] = 0
 data['ema_winrate'] = 0
-ema_winrate = 0 
 
 def percent_change(df, i):  #(new_price-old_price)/old_price *100
      return ((df.iloc[i]-df.iloc[i-1])/df.iloc[i-1])*100
 
-for i in range(len(data)):
-    if data['ema50'].iloc[i] >= data['ema200'].iloc[i]:
-        data['crossing'].iloc[i] = 10
-        if percent_change(data['Adj Close'],i) >= 5:       #+5%
-            ema_winrate=ema_winrate+1
-            data['ema_winrate'].iloc[i]=20
-    if percent_change(data['Adj Close'],i) <= -5:       #-5%
-            ema_winrate=ema_winrate+1
-            data['ema_winrate'].iloc[i]=20
+#plots winrate from price, 2 inputs
+def winrate_plotter(df_price, df_crossing, df_in_1, df_in_2, df_winrate):
+    winrate = 0
+    for i in range(len(df_price)):
+        if df_in_1.iloc[i] >= df_in_2.iloc[i]:
+            df_crossing.iloc[i] = 10
+            if percent_change(df_price,i) >= 5:       #+5%
+                winrate=winrate+1
+                df_winrate.iloc[i]=20
+        if percent_change(df_price,i) <= -5:       #-5%
+                winrate=winrate+1
+                df_winrate.iloc[i]=20
+    return winrate, df_winrate
 
+ema_winrate, data['ema_winrate'] = winrate_plotter(data['Adj Close'], data['crossing'], data['ema50'], data['ema200'], data['ema_winrate'])
 
 print(data)
 print('ema_winrate:',ema_winrate ,'/',len(data))
