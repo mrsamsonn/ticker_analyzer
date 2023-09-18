c99cc2eeabfc48d60ef79f427749f30ea1e5222e
mrsamsonn <andrewsj.ahs@gmail.com>
2023-09-18 12:06:01 -0700

    static ema plots

--- ticker_analyer.py
+++ ticker_analyer.py
@@ -2,6 +2,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+#import ta_py as ta
+import numpy as np
+import talib as ta
 
 # while get_live_price('SPY') != '':
 #     print(get_live_price('SPY'))
@@ -10,9 +13,13 @@ import yfinance as yf
 #test static analysis first
 
 data = yf.download('SPY')
+data['ema50'] = ta.EMA(data["Adj Close"], 50) 
+data['ema200'] = ta.EMA(data["Adj Close"], 200)
+
 print(data)
 
 
 plt.plot(data.index, data['Adj Close'])
+plt.plot(data.index,data['ema50'])
+plt.plot(data.index,data['ema200'])
 plt.show()
-
