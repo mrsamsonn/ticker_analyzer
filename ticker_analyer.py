fb3899a26ad010d38ee765b0d3bde311f0d7f0cc
mrsamsonn <andrewsj.ahs@gmail.com>
2023-09-17 17:24:58 -0700

    installed yfinance for static data

--- ticker_analyer.py
+++ ticker_analyer.py
@@ -1,4 +1,11 @@
-from yahoo_fin.stock_info import *
+# from yahoo_fin.stock_info import *
+import pandas as pd
+import matplotlib.pyplot as plt
 
-while get_live_price('SPY') != '':
-    print(get_live_price('SPY'))
+# while get_live_price('SPY') != '':
+#     print(get_live_price('SPY'))
+
+#################
+#test static analysis first
+
+
