943d081218e16f235c8cd775ce91a25ed9a0b715
mrsamsonn <andrewsj.ahs@gmail.com>
2023-09-18 15:17:51 -0700

    loop cleanup

--- ticker_analyer.py
+++ ticker_analyer.py
@@ -12,18 +12,18 @@ import talib as ta
 #################
 #test static analysis first
 
+#vars
 data = yf.download('SPY')
 data['ema50'] = ta.EMA(data["Adj Close"], 50) 
 data['ema200'] = ta.EMA(data["Adj Close"], 200)
 data['crossing'] = 0
-data['ema_winrate'] = 0 
+data['ema_winrate'] = 0
+ema_winrate = 0 
 
 def percent_change(df, i):  #(new_price-old_price)/old_price *100
      return ((df.iloc[i]-df.iloc[i-1])/df.iloc[i-1])*100
 
-i=0
-ema_winrate=0
-while i != len(data):
+for i in range(len(data)):
     if data['ema50'].iloc[i] >= data['ema200'].iloc[i]:
         data['crossing'].iloc[i] = 10
         if percent_change(data['Adj Close'],i) >= 5:       #+5%
@@ -32,7 +32,7 @@ while i != len(data):
     if percent_change(data['Adj Close'],i) <= -5:       #-5%
             ema_winrate=ema_winrate+1
             data['ema_winrate'].iloc[i]=20
-    i=i+1
+    # i=i+1
 
 
 print(data)
