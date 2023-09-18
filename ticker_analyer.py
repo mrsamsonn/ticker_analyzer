4cd491bf6148774e4b8bcc50405a17a5dc58d4d6
mrsamsonn <andrewsj.ahs@gmail.com>
2023-09-18 14:00:39 -0700

    calc freq when price change is 5% at ema cross

--- ticker_analyer.py
+++ ticker_analyer.py
@@ -16,23 +16,27 @@ data = yf.download('SPY')
 data['ema50'] = ta.EMA(data["Adj Close"], 50) 
 data['ema200'] = ta.EMA(data["Adj Close"], 200)
 data['crossing'] = 0
+data['ema_winrate'] = 0 
 
 
 i=0
+ema_winrate=0
 while i != len(data):
     if data['ema50'].iloc[i] >= data['ema200'].iloc[i]:
         data['crossing'].iloc[i] = 10
+        if (((data['Adj Close'].iloc[i]-data['Adj Close'].iloc[i-1])/data['Adj Close'].iloc[i-1])*100) >= 5:       #(new_price-old_price)/old_price *100
+            ema_winrate=ema_winrate+1
+            data['ema_winrate'].iloc[i]=20
     i=i+1
 
 
-    
-
 print(data)
-
+print('ema_winrate:',ema_winrate ,'/',len(data))
 
 plt.plot(data.index, data['Adj Close'], label='Closing Price')
 plt.plot(data.index,data['ema50'], label='EMA_50')
 plt.plot(data.index,data['ema200'], label='EMA_200')
 plt.plot(data.index,data['crossing'], label='EMA_50/200_Crossing')
+plt.plot(data.index,data['ema_winrate'], label='ema_wins +5% price change', color='cyan')
 plt.legend(loc="upper left")
 plt.show()
