65270a491f51b602b46987ed8a1b92a7beea4a1b
mrsamsonn <andrewsj.ahs@gmail.com>
2023-09-18 17:05:33 -0700

    macd histogram + macd_based winrate

--- ticker_analyer.py
+++ ticker_analyer.py
@@ -9,16 +9,12 @@ import talib as ta
 # while get_live_price('SPY') != '':
 #     print(get_live_price('SPY'))
 
-#################
-#test static analysis first
+###########################################
+########test static analysis first#########
+###########################################
 
-#vars
-data = yf.download('SPY')
-data['ema50'] = ta.EMA(data["Adj Close"], 50) 
-data['ema200'] = ta.EMA(data["Adj Close"], 200)
-data['crossing'] = 0
-data['ema_winrate'] = 0
-
+##############################
+#function declarations
 def percent_change(df, i):  #(new_price-old_price)/old_price *100
      return ((df.iloc[i]-df.iloc[i-1])/df.iloc[i-1])*100
 
@@ -36,15 +32,49 @@ def winrate_plotter(df_price, df_crossing, df_in_1, df_in_2, df_winrate):
                 df_winrate.iloc[i]=20
     return winrate, df_winrate
 
+################################
+
+#vars
+data = yf.download('SPY')
+data['ema50'] = ta.EMA(data["Adj Close"], 50) 
+data['ema200'] = ta.EMA(data["Adj Close"], 200)
+data['macd'], data['macdsignal'], data['macdhist'] = ta.MACD(data["Adj Close"], 12, 26, 9)
+data['crossing'] = 0
+data['ema_winrate'] = 0
+data['macd_winrate'] = 0
+macd_winrate = 0
+
 ema_winrate, data['ema_winrate'] = winrate_plotter(data['Adj Close'], data['crossing'], data['ema50'], data['ema200'], data['ema_winrate'])
+
+for i in range(len(data)):
+    if data['macdhist'].iloc[i] >= 0:
+        if percent_change(data['Adj Close'],i) >= 5:
+            macd_winrate=macd_winrate+1
+            data['macd_winrate'].iloc[i]=30
+    else:
+         if percent_change(data['Adj Close'],i) <= -5:
+            macd_winrate=macd_winrate+1
+            data['macd_winrate'].iloc[i]=30
+     
+             
 
 print(data)
 print('ema_winrate:',ema_winrate ,'/',len(data))
+print('macd_winrate:',macd_winrate ,'/',len(data))
 
-plt.plot(data.index, data['Adj Close'], label='Closing Price')
-plt.plot(data.index,data['ema50'], label='EMA_50')
-plt.plot(data.index,data['ema200'], label='EMA_200')
-plt.plot(data.index,data['crossing'], label='EMA_50/200_Crossing')
-plt.plot(data.index,data['ema_winrate'], label='ema_wins +-5% price change', color='cyan')
-plt.legend(loc="upper left")
+figure, axis = plt.subplots(2)
+
+axis[0].plot(data.index, data['Adj Close'], label='Closing Price')
+axis[0].plot(data.index,data['ema50'], label='EMA_50')
+axis[0].plot(data.index,data['ema200'], label='EMA_200')
+axis[0].plot(data.index,data['macd_winrate'], label='macd_wins +-5% price change', color='pink')
+#axis[0].plot(data.index,data['crossing'], label='EMA_50/200_Crossing')
+axis[0].plot(data.index,data['ema_winrate'], label='ema_wins +-5% price change', color='cyan')
+axis[0].legend(loc="upper left")
+
+# axis[1].plot(data.index,data['macd'], label='macd')
+# axis[1].plot(data.index,data['macdsignal'], label='macdsignal')
+colors = ['g' if i >= 0 else 'r' for i in data['macdhist']] #for histogram color
+axis[1].bar(data.index, data['macdhist'], label='macdhist', color=colors)
+axis[1].legend(loc="upper left")
 plt.show()
